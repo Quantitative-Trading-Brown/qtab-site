6c43c474c03d46d2b8d3788ee1c4ea7fafca5ed0
Alan W <awangc31s@gmail.com>
2025-01-27 18:28:17 -0500

    That sweet sweet underline

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,17 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Header = () => {
+  const thisPath = usePathname();
+
+  const siteLinks = {
+    "/": "Home",
+    "/events": "Events",
+    "/team": "Team",
+    "/sponsors": "Sponsors",
+  };
+
   return (
     <header className="bg-black text-white">
       <div className="container flex items-center justify-between py-4 px-10">
@@ -13,11 +24,14 @@ const Header = () => {
             />
           </Link>
         </div>
+
         <nav className="flex space-x-6">
-          <Link href="/">Home</Link>
-          <Link href="/events">Events</Link>
-          <Link href="/team">Team</Link>
-          <Link href="/sponsors">Sponsors</Link>
+          {Object.entries(siteLinks).map(([path, name]) => (
+            <Link key={name} href={path} className="underline-offset-[7px]">
+              {thisPath == path ? <u>{name}</u> : <>{name}</>}
+            </Link>
+          ))}
+
           <Link
             href="https://docs.google.com/forms/d/e/1FAIpQLSdvymh0QrCLnqUdC4NeC7qVTdYzAei0YQ-sRbf1nf6D3nJVZQ/viewform?usp=sf_link"
             target="_blank"
@@ -29,7 +43,7 @@ const Header = () => {
             <button className="group">Resources</button>
             <div
               className="hidden group-hover:flex hover:flex flex-col 
-            bg-white text-black rounded absolute p-2 z-20 w-[10em]"
+              bg-white text-black rounded absolute p-2 z-20 w-[10em]"
             >
               <Link
                 href="https://youtube.com/playlist?list=PLQIIHL3oP4cxA502sW5YI8czUOAZB73Bn&si=kSp9CzEBrXrnenqy"
@@ -49,6 +63,7 @@ const Header = () => {
               </Link>
             </div>
           </div>
+
           <Link
             href="https://browninvestmentgroup.co"
             target="_blank"
