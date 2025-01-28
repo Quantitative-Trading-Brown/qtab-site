06cce96c0914e62b2c0776cca59988c03f5cd9e9
ryane008 <ryan_eng@brown.edu>
2025-01-28 14:54:16 -0500

    stuff

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,40 +1,49 @@
-export default function Home() {
+"use client";
+import React, { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import db from "@/scripts/firestore";
+
+
+export default function Events() {
+  const [info, setData] = useState<Object>("");
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, "events"));
+        const documents = querySnapshot.docs;
+        const data = Object.fromEntries(
+          documents.map((doc) => [doc.id, doc.data()])
+        );
+        setData(data);
+      } catch (error) {
+        console.error("Error fetching Firestore data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="flex-grow items-center relative py-20">
-      <div className="overflow-hidden whitespace-nowrap relative py-4">
-        <div className="flex animate-marquee">
-          <span className="mr-8 text-5xl">
-            Mathematicians. Innovators. Researchers. Traders. Developers. Thinkers. Creators. Learners. 
-            Coders. Statisticians. Builders. Teachers. Programmers. Quants. Scientists. Technologists. Engineers.
-          </span>
-          <span className="mr-8 text-5xl">
-            Mathematicians. Innovators. Researchers. Traders. Developers. Thinkers. Creators. Learners. 
-            Coders. Statisticians. Builders. Teachers. Programmers. Quants. Scientists. Technologists. Engineers.
-          </span>
-        </div>
-      </div>
-      <div className="flex mt-10">
-        <div className="p-10">
-          <div className="flex flex-col items-center relative z-10 text-center p-5">
-            <h1 className="text-5xl font-bold">
-              Welcome to Quantitative Trading at Brown
-            </h1>
-            <p className="mt-4 text-xl w-[75%]">
-              A student-run quantitative fund dedicated to education,
-              innovation, and growth.
-            </p>
-            <a
-              href="/"
-              className="inline-block bg-blue-600 hover:bg-blue-700 text-white font-semibold py-3 px-6 mt-6 rounded-lg"
-            >
-              Learn More
-            </a>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      {Object.values(info).map((resource, index) => (
+        <div key={index} className="bg-white shadow-md rounded-lg p-6 flex">
+          <img
+            className="w-48 h-48 object-cover rounded-none border-2 border-indigo-500 mr-4"
+            src={resource.image}
+            alt={resource.name}
+          />
+          <div className="flex flex-col justify-center">
+            <h2 className="text-gray-800 text-lg font-semibold mb-2">
+              {resource.name}
+            </h2>
+            <p className="text-gray-600">{resource.description}</p>
           </div>
         </div>
-        <div className="p-7">
-          <img src="/images/erc.jpg" alt="Brown ERC" />
-        </div>
-      </div>
+      ))}
     </div>
   );
 }
