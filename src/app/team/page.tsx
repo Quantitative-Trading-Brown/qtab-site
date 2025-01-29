190d6bc2ce5d11fbde4336f101761ccc5e369d4a
ryane008 <ryan_eng@brown.edu>
2025-01-28 21:09:44 -0500

    stuff

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -1,95 +1,188 @@
+"use client";
 import mockData from "./mock/mock_data.json";
+import React from "react";
+import { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import db from "@/scripts/firestore";
+
 interface ResourceData {
   name: string;
   image: string;
-  description: string;
+  desc: string;
+  index: number;
+  display: boolean;
+  position: string;
 }
 
 type DataType = {
     [key: string]: ResourceData;
 };
 
+
 export default function Team() {
-    const mockDataTyped = mockData as DataType;
+    const [info, setData] = useState<DataType>({});
+    const [loading, setLoading] = useState(true);
+
+    const [isOpen, setIsOpen] = useState(false);
+    const [selectedResource, setSelectedResource] = useState<ResourceData>();
+
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const querySnapshot = await getDocs(collection(db, "team"));
+          const documents = querySnapshot.docs;
+          const data = Object.fromEntries(
+            documents.map((doc) => [doc.id, doc.data()])
+          );
+          console.log(data);
+          setData(data);
+          console.log("Hello", Object.values(data));
+        } catch (error) {
+          console.error("Error fetching Firestore data:", error);
+        } finally {
+          setLoading(false);
+        }
+      };
+
+      fetchData();
+    }, []);
+    
     return (
       <section className="flex flex-col items-center justify-center">
-        <div className="flex flex-col items-center justify-center">
+        <div className="flex flex-col items-center justify-center pb-10">
           <u>
-            <h1 className="text-4xl py-10 mb-8">Leadership</h1>
+            <h1 className="text-4xl py-10 mb-8">2024-2025 Leadership</h1>
           </u>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {Object.values(mockDataTyped).map((resource, index) => (
-              <div
-                key={index}
-                className="bg-white shadow-md rounded-lg p-6 flex"
-              >
-                <img
-                  className="w-48 h-48 object-cover rounded-none border-2 border-indigo-500 mr-4"
-                  src={resource.image}
-                  alt={resource.name}
-                />
-                <div className="flex flex-col justify-center">
-                  <h2 className="text-gray-800 text-lg font-semibold mb-2">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 px-10">
+            {Object.values(info)
+              .filter(
+                (resource) =>
+                  resource.display && resource.position == "Leadership"
+              )
+              .sort((a, b) => a.index - b.index)
+              .map((resource, index) => (
+                <div
+                  key={index}
+                  className="bg-white shadow-md rounded-lg p-6 cursor-pointer flex flex-col items-center max-w-sm"
+                  onClick={() => {
+                    setSelectedResource(resource);
+                    setIsOpen(true);
+                  }}
+                >
+                  <img
+                    className="w-48 h-48 object-cover rounded-none border-2 border-indigo-500 mb-4 mx-auto"
+                    src={resource.image}
+                    alt={resource.name}
+                  />
+                  <h2 className="text-gray-800 text-lg font-semibold mb-2 text-center py-4">
                     {resource.name}
                   </h2>
-                  <p className="text-gray-600">{resource.description}</p>
+                  <p className="text-gray-600 text-center px-4">
+                    {resource.desc.length > 100
+                      ? `${resource.desc.slice(0, 100)}...`
+                      : resource.desc}
+                  </p>
                 </div>
-              </div>
-            ))}
+              ))}
           </div>
-        </div>
 
-        <div className="flex flex-col items-center justify-center">
-          <u>
-            <h1 className="text-4xl py-10 mb-8">Researchers and Engineers</h1>
-          </u>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {Object.values(mockDataTyped).map((resource, index) => (
-              <div
-                key={index}
-                className="bg-white shadow-md rounded-lg p-6 flex"
-              >
+          {isOpen && selectedResource && (
+            <div className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50">
+              <div className="bg-white rounded-lg p-8 relative max-w-3xl w-full mx-4">
+                <button
+                  className="absolute top-4 right-4 text-gray-700 text-2xl font-bold"
+                  onClick={() => setIsOpen(false)}
+                >
+                  &times;
+                </button>
                 <img
-                  className="w-48 h-48 object-cover rounded-none border-2 border-indigo-500 mr-4"
-                  src={resource.image}
-                  alt={resource.name}
+                  className="w-full h-auto object-cover rounded-none border-2 border-indigo-500 mb-6"
+                  src={selectedResource.image}
+                  alt={selectedResource.name}
                 />
-                <div className="flex flex-col justify-center">
-                  <h2 className="text-gray-800 text-lg font-semibold mb-2">
-                    {resource.name}
-                  </h2>
-                  <p className="text-gray-600">{resource.description}</p>
-                </div>
+                <h2 className="text-2xl font-semibold mb-4 text-center">
+                  {selectedResource.name}
+                </h2>
+                <p className="text-gray-700">{selectedResource.desc}</p>
               </div>
-            ))}
-          </div>
-        </div>
+            </div>
+          )}
 
-        <div className="flex flex-col items-center justify-center">
-          <u>
-            <h1 className="text-4xl py-10 mb-8">Advisors</h1>
-          </u>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {Object.values(mockDataTyped).map((resource, index) => (
-              <div
-                key={index}
-                className="bg-white shadow-md rounded-lg p-6 flex"
-              >
-                <img
-                  className="w-48 h-48 object-cover rounded-none border-2 border-indigo-500 mr-4"
-                  src={resource.image}
-                  alt={resource.name}
-                />
-                <div className="flex flex-col justify-center">
-                  <h2 className="text-gray-800 text-lg font-semibold mb-2">
-                    {resource.name}
-                  </h2>
-                  <p className="text-gray-600">{resource.description}</p>
-                </div>
-              </div>
-            ))}
+          <div className="flex flex-col items-center justify-center">
+            <u>
+              <h1 className="text-4xl py-10 mb-8">
+                2024-2025 Researchers and Engineers
+              </h1>
+            </u>
+            <div className="grid grid-cols-1 gap-6 px-10 justify-items-center">
+              {Object.values(info)
+                .filter(
+                  (resource) =>
+                    resource.display &&
+                    resource.position == "Researchers and Engineers"
+                )
+                .sort((a, b) => a.index - b.index)
+                .map((resource, index) => (
+                  <div
+                    key={index}
+                    className="bg-white shadow-md rounded-lg p-6 cursor-pointer flex flex-col items-center max-w-sm"
+                    onClick={() => {
+                      setSelectedResource(resource);
+                      setIsOpen(true);
+                    }}
+                  >
+                    <img
+                      className="w-48 h-48 object-cover rounded-none border-2 border-indigo-500 mb-4 mx-auto"
+                      src={resource.image}
+                      alt={resource.name}
+                    />
+                    <h2 className="text-gray-800 text-lg font-semibold mb-2 text-center py-4">
+                      {resource.name}
+                    </h2>
+                  </div>
+                ))}
+            </div>
           </div>
+
+          <div className="flex flex-col items-center justify-center">
+            <u>
+              <h1 className="text-4xl py-10 mb-8">Advisors</h1>
+            </u>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 px-10">
+              {Object.values(info)
+                .filter(
+                  (resource) =>
+                    resource.display && resource.position == "Advisor"
+                )
+                .sort((a, b) => a.index - b.index)
+                .map((resource, index) => (
+                  <div
+                    key={index}
+                    className="bg-white shadow-md rounded-lg p-6 cursor-pointer flex flex-col items-center max-w-sm"
+                    onClick={() => {
+                      setSelectedResource(resource);
+                      setIsOpen(true);
+                    }}
+                  >
+                    <img
+                      className="w-48 h-48 object-cover rounded-none border-2 border-indigo-500 mb-4 mx-auto"
+                      src={resource.image}
+                      alt={resource.name}
+                    />
+                    <h2 className="text-gray-800 text-lg font-semibold mb-2 text-center py-4">
+                      {resource.name}
+                    </h2>
+                    <p className="text-gray-600 text-center px-4">
+                      {resource.desc.length > 100
+                        ? `${resource.desc.slice(0, 100)}...`
+                        : resource.desc}
+                    </p>
+                  </div>
+                ))}
+            </div>
+          </div>
+
         </div>
       </section>
     );
