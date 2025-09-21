62272d5ddde88ffa08049b70993a1b39f3090d5e
Alan W <awangc31s@gmail.com>
2025-09-20 21:32:03 -0400

    Fixed marquee

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,15 @@
 import Link from "next/link";
+import Marquee from "react-fast-marquee";
+
 export default function Home() {
   return (
     <div className="flex-grow items-center relative py-10">
       <div>
-        <div className="overflow-hidden whitespace-nowrap py-4">
-          <div className="flex animate-marquee">
-            <span className="text-5xl">
-              Mathematicians. Innovators. Researchers. Traders. Developers.
-              Thinkers. Creators. Learners. Coders. Statisticians. Builders.
-              Teachers. Programmers. Quants. Scientists. Technologists.
-              Engineers. Mathematicians. Innovators. Researchers. Traders. Developers.
-              Thinkers. Creators. Learners. Coders. Statisticians. Builders.
-              Teachers. Programmers. Quants. Scientists. Technologists.
-              Engineers.
-            </span>
-          </div>
-        </div>
+        <Marquee speed="300" className="text-5xl">
+          Mathematicians. Innovators. Researchers. Traders. Developers.
+          Thinkers. Creators. Learners. Coders. Statisticians. Builders.
+          Teachers. Programmers. Quants. Scientists. Technologists. Engineers.&nbsp;
+        </Marquee>
         <div className="flex mt-20 px-20 items-center">
           <div className="p-10">
             <div className="flex flex-col justify-center items-center relative z-10 text-center p-5">
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,15 +13,6 @@ export default {
         helvetica: ['Helvetica Neue', 'Helvetica'],
         montserrat: ['Montserrat', 'sans-serif'],
       },
-      animation: {
-        marquee: "marquee 10s linear infinite",
-      },
-      keyframes: {
-        marquee: {
-          "0%": { transform: "translateX(0%)" },
-          "100%": { transform: "translateX(-50%)" },
-        },
-      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
