b6c3e3305f3d6fa7b6bf50c5cbc69a47a0ad7f74
Alan W <awangc31s@gmail.com>
2025-09-20 21:41:04 -0400

    Changed name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,18 +5,18 @@ export default function Home() {
   return (
     <div className="flex-grow items-center relative py-10">
       <div>
-        <Marquee speed="300" className="text-5xl">
+        <Marquee speed="200" className="text-5xl">
           Mathematicians. Innovators. Researchers. Traders. Developers.
           Thinkers. Creators. Learners. Coders. Statisticians. Builders.
           Teachers. Programmers. Quants. Scientists. Technologists. Engineers.&nbsp;
         </Marquee>
         <div className="flex mt-20 px-20 items-center">
-          <div className="p-10">
+          <div className="flex-grow p-10">
             <div className="flex flex-col justify-center items-center relative z-10 text-center p-5">
               <h1 className="text-5xl font-bold">
-                Welcome to Quantitative Trading at Brown
+                Welcome to Quant Trading at Brown
               </h1>
-              <p className="mt-4 text-xl w-[80%]">
+              <p className="mt-4 text-xl w-[60%]">
                 A club for math and CS enthusiasts dedicated to education,
                 innovation, and growth.
               </p>
